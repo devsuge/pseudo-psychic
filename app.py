1426be59974eee4cd559c41c3332b5fa9c9e0422
suqah <getretardisfree@gmail.com>
2021-12-28 23:51:53 +0700

    fix logic

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, session
-import psychics
+from flask.views import View
+import logic
 import os
+import pickle
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(20).hex()
@@ -8,61 +10,36 @@ app.config['SECRET_KEY'] = os.urandom(20).hex()
 
 @app.route('/', methods=['GET'])
 def index():
-    if not('history_digit' in session):
-        session['history_digit'] = []
-    if not ('proxy_list' in session):
-        session['proxy_list'] = [0, 0]
-    if not('history_psychic1' in session):
-        session['history_psychic1'] = []
-    if not('history_psychic2' in session):
-        session['history_psychic2'] = []
-
+    if not('Player' in session):
+        session['Player'] = pickle.dumps(logic.Player())
     return render_template('index.html', session=session)
 
 
 @app.route('/predict', methods=['GET'])
 def predict():
-    answer_1 = psychics.random_psychic.predict()
-
-    if len(session['history_psychic2']) == 0:
-        answer_2 = 10
-    else:
-        answer_2 = psychics.range_psychic.predict(session['history_psychic2'][-1])
-
-    if (len(session['history_psychic1']) - len(session['history_digit'])) == 1:
-        session['history_psychic1'][-1] = answer_1
-        session['history_psychic2'][-1] = answer_2
-    else:
-        session['history_psychic1'].append(answer_1)
-        session['history_psychic2'].append(answer_2)
+    pl = pickle.loads(session['Player'])
+    pl.ask_psychics()
+    session['Player'] = pickle.dumps(pl)
 
     if not session.modified:
         session.modified = True
 
-    return render_template('predict.html', session=session, len=len(session['history_digit']))
+    return render_template('predict.html', Player=pl, len=len(pl.history))
 
 
 @app.route('/answer', methods=['POST', 'GET'])
 def answer():
     if request.method == "POST":
+        pl = pickle.loads(session['Player'])
         digit = int(request.form['digit'])
-        session['history_digit'].append(digit)
-
-        if session['history_psychic1'][-1] == digit:
-            session['proxy_list'][0] += 1
-        else:
-            session['proxy_list'][0] -= 1
-
-        if session['history_psychic2'][-1] == digit:
-            session['proxy_list'][1] += 1
-        else:
-            session['proxy_list'][1] -= 1
+        pl.check_predict(digit)
+        session['Player'] = pickle.dumps(pl)
 
         if not session.modified:
             session.modified = True
 
-        return render_template('index.html', session=session)
-    return render_template('answer.html', session=session)
+        return render_template('index.html')
+    return render_template('answer.html')
 
 
 if __name__ == '__main__':
---
+++ logic.py
@@ -0,0 +1,45 @@
+# psychic list
+import random
+
+
+class RandomPsychic(object):
+    predict = int()
+    history = []
+
+    def get_predict(self):
+        self.predict = random.randint(10, 99)
+
+    def save_predict(self):
+        self.history.append(self.predict)
+
+
+class RangePsychic(RandomPsychic):
+    predict = 9
+    history = []
+
+    def get_predict(self):
+        if RangePsychic.predict < 99:
+            self.predict = RangePsychic.predict + 1
+            self.history.append(self.predict)
+        else:
+            self.predict = 10
+            self.history.append(self.predict)
+
+
+class Player(object):
+    history = []
+    psychics = [RandomPsychic(), RangePsychic()]
+    proxy = [0, 0]
+
+    def ask_psychics(self):
+        for i in range(len(Player.psychics)):
+            self.psychics[i].get_predict()
+
+    def check_predict(self, digit):
+        self.history.append(digit)
+        for i in range(len(Player.psychics)):
+            if self.psychics[i].predict == digit:
+                self.proxy[i] += 1
+            else:
+                self.proxy[i] -= 1
+            self.psychics[i].save_predict()
